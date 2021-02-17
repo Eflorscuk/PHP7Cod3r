05ebdfd35408040c04ff9a38453814840ed17107
Evandro Florscuk <evandro_ff@me.com>
2021-02-17 16:51:20 -0300

    Finalização da pasta include e atualização do index.html

---
+++ includes/include_once.php
@@ -0,0 +1,13 @@
+<div class="titulo">Include Once</div>
+
+<?php
+ini_set('display_errors', 1);
+
+include_once('include_once_arquivo.php');
+//require('include_once_arquivo.php');
+
+echo "Variável = '{$variavel}'.<br>";
+$variavel = 'Variável Alterada';
+echo "Variável = '{$variavel}'.<br>";
+
+//include('include_once_arquivo.php');
